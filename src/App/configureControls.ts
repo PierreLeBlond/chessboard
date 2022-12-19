fbf37a6a8b23fcba698dff5387a6feadd64c108c
Lespingal <pierre.lespingal@gmail.com>
2022-12-19 17:52:19 +0100

    fix: Fix controls initialization

--- src/App/configureControls.ts
+++ src/App/configureControls.ts
@@ -4,8 +4,8 @@ export default function configureControls(viewer: Viewer) {
   const { controls } = viewer;
   controls.enablePan = false;
   controls.rotateSpeed = 0.5;
-  controls.minDistance = 1;
-  controls.maxDistance = 10;
+  controls.minDistance = 10;
+  controls.maxDistance = 15;
   controls.maxPolarAngle = 1.5;
   controls.enableDamping = true;
   controls.dampingFactor = 0.1;
