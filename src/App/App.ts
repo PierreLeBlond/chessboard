811ac90d6923d5653795392615be5a0376fe0fb7
PierreLeBlond <pierre.lespingal@gmail.com>
2024-06-26 11:35:32 +0200

    chore: Update dependencies

--- src/App/App.ts
+++ src/App/App.ts
@@ -1,15 +1,14 @@
-import { PublicViewer, Reflector, THREE, Scene } from '@s0rt/3d-viewer';
+import { PublicViewer, Reflector, THREE, Scene } from "@s0rt/3d-viewer";
 
-import chessboard from './assets/chess.gltf';
-import configureControls from './configureControls';
+import chessboard from "./assets/chess.gltf";
+import configureControls from "./configureControls";
 
-export default class App extends THREE.EventDispatcher {
+export default class App {
   public viewer: PublicViewer;
 
   private scene: Scene | null = null;
 
   constructor(viewer: PublicViewer) {
-    super();
     this.viewer = viewer;
   }
 
@@ -23,8 +22,8 @@ export default class App extends THREE.EventDispatcher {
 
       this.viewer.addTasks({
         parallelTasks: [
-          { task: async () => await this.scene?.loadAsset(chessboard) }
-        ]
+          { task: async () => await this.scene?.loadAsset(chessboard) },
+        ],
       });
       await this.viewer.launchTasks();
       this.setBoardReflections();
@@ -36,11 +35,13 @@ export default class App extends THREE.EventDispatcher {
 
     configureControls(this.viewer.viewer);
     this.setCameraPosition();
-    (scene.getObjectByName('chess-reflector') as Reflector).start();
+    (scene.getObjectByName("chess-reflector") as Reflector).start();
   }
 
   public stop() {
-    (this.viewer.viewer.scene.getObjectByName('chess-reflector') as Reflector).stop();
+    (
+      this.viewer.viewer.scene.getObjectByName("chess-reflector") as Reflector
+    ).stop();
   }
 
   private setCameraPosition() {
@@ -58,10 +59,11 @@ export default class App extends THREE.EventDispatcher {
   }
 
   private setPawnAnimationOffets() {
-    this.scene?.getAllAnimations()
+    this.scene
+      ?.getAllAnimations()
       .filter((animationMixer: THREE.AnimationMixer) => {
         const mesh = animationMixer.getRoot() as THREE.Mesh;
-        return mesh.name.includes('Pawn') && mesh.type == 'SkinnedMesh';
+        return mesh.name.includes("Pawn") && mesh.type == "SkinnedMesh";
       })
       .forEach((animationMixer: THREE.AnimationMixer, index: number) => {
         animationMixer.setTime(index / 16);
@@ -69,23 +71,27 @@ export default class App extends THREE.EventDispatcher {
   }
 
   private setBoardReflections() {
-
     if (!this.scene) {
-      throw new Error('Scene does not exists');
+      throw new Error("Scene does not exists");
     }
 
-    const board =
-      this.viewer.viewer.scene.getObjectByName('Chessboard') as THREE.Mesh;
+    const board = this.viewer.viewer.scene.getObjectByName(
+      "Chessboard"
+    ) as THREE.Mesh;
 
-    const reflector = new Reflector(
-      this.viewer.viewer, this.scene, board, { textureWidth: 1024, textureHeight: 1024 });
+    const reflector = new Reflector(this.viewer.viewer, this.scene, board, {
+      textureWidth: 1024,
+      textureHeight: 1024,
+    });
     reflector.name = "chess-reflector";
     reflector.rotateX(-Math.PI / 2);
     this.viewer.viewer.scene.add(reflector);
   }
 
   public dispose() {
-    (this.viewer.viewer.scene.getObjectByName('chess-reflector') as Reflector).dispose();
+    (
+      this.viewer.viewer.scene.getObjectByName("chess-reflector") as Reflector
+    ).dispose();
     this.viewer.dispose();
   }
 }
